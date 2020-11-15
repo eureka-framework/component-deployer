397cc046edc1b641803b85f4462e5349718a215e
velkuns <romain.inscription@hotmail.fr>
2020-11-15 23:57:04 +0100

    Add script to initialize some symlinks

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,10 @@ The format is based on [Keep a Changelog](http://keepachangelog.com/en/1.0.0/)
 and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.html).
 
 
+## [1.2.0] - Unreleased 
+### Add
+ * Script to initialize some symlinks
+
 ## [1.1.0] - 2020-11-06
 ### Changed
  * Minor changes in deployment scripts
---
+++ src/Script/Install/Init/Symlink.php
@@ -0,0 +1,62 @@
+<?php
+
+/*
+ * Copyright (c) Romain Cottard
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Eureka\Component\Deployer\Script\Install\Init;
+
+use Eureka\Component\Deployer\Common\AbstractCommonScript;
+
+/**
+ * Class Link
+ *
+ * @author Romain Cottard
+ * @codeCoverageIgnore
+ */
+class Symlink extends AbstractCommonScript
+{
+    /**
+     * Directories constructor.
+     */
+    public function __construct()
+    {
+        $this->setDescription('Initializing links (symlinks)');
+        $this->setExecutable(true);
+    }
+
+    /**
+     * @return void
+     */
+    public function run(): void
+    {
+        $this->chdirSource();
+
+        $this->createSymlinks();
+    }
+
+    /**
+     * @return void
+     */
+    private function createSymlinks(): void
+    {
+        $rootDir = $this->getRootDirSource();
+
+        $this->displayInfo('Create symlinks');
+        foreach ($this->config['install']['init']['symlinks'] as $source => $destination) {
+            $destination = str_replace('//', '/', $rootDir . DIRECTORY_SEPARATOR . $destination);
+
+            if (!symlink($source, $destination)) {
+                $this->displayInfoFailed();
+                $this->throw("Could not create symlink from ${source} to ${destination}");
+            }
+        }
+
+        $this->displayInfoDone();
+    }
+}
